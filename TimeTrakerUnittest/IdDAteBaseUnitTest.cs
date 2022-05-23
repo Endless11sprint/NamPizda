71f6de9cfd520cb77b128613f5559e849fe2a685
Endless11sprint <94888543+Endless11sprint@users.noreply.github.com>
2022-05-24 01:26:24 +0300

    Add files via upload

---
+++ TimeTrakerUnittest/IdDAteBaseUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeTraker;
+using System;
+
+
+namespace TimeTrakerUnittest
+{
+    [TestClass]
+    public class IdDAteBaseUnitTest
+    {
+        [TestMethod]
+        public void DateStartStringTest()
+        {
+            // arange
+            
+            TasksOver tasksOver = new TasksOver(40, "ДаваймногоДней", "ага", new DateTime(2022, 5, 15, 1, 1, 58), new DateTime(2022, 5, 17, 1, 1, 58));
+            string resultEX = "15/5/2022";
+            // act
+
+            string relust = tasksOver.DateStartString();
+
+            // assert
+
+            Assert.AreEqual(resultEX, relust);
+        }
+
+
+    }
+}
